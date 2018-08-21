f340c621a41861f0acb0110698898fcbcc6fcfbe
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-08-21 16:27:45 +0800

    added directions constants

---
+++ src/model/directions.js
@@ -0,0 +1,11 @@
+const NORTH = "N";
+const SOUTH = "S";
+const EAST = "E";
+const WEST = "W";
+
+module.exports = {
+  NORTH,
+  SOUTH,
+  EAST,
+  WEST
+};
--- src/model/rover.js
+++ src/model/rover.js
@@ -1,4 +1,5 @@
-const { LEFT, RIGHT } = require("../model/command");
+const { LEFT, RIGHT } = require("./command");
+const { NORTH, SOUTH, EAST, WEST } = require("./directions");
 
 class Rover {
   constructor(currentLocation, currentHeading) {
@@ -16,37 +17,37 @@ class Rover {
   }
 
   moveForward() {
-    if (this.currentHeading === "E") {
+    if (this.currentHeading === EAST) {
       this.currentLocation[0] += 1;
-    } else if (this.currentHeading === "W") {
+    } else if (this.currentHeading === WEST) {
       this.currentLocation[0] -= 1;
-    } else if (this.currentHeading === "N") {
+    } else if (this.currentHeading === NORTH) {
       this.currentLocation[1] += 1;
-    } else if (this.currentHeading === "S") {
+    } else if (this.currentHeading === SOUTH) {
       this.currentLocation[1] -= 1;
     }
   }
 
   changeHeading(direction) {
     if (direction === LEFT) {
-      if (this.currentHeading === "N") {
-        this.currentHeading = "W";
-      } else if (this.currentHeading === "W") {
-        this.currentHeading = "S";
-      } else if (this.currentHeading === "S") {
-        this.currentHeading = "E";
-      } else if (this.currentHeading === "E") {
-        this.currentHeading = "N";
+      if (this.currentHeading === NORTH) {
+        this.currentHeading = WEST;
+      } else if (this.currentHeading === WEST) {
+        this.currentHeading = SOUTH;
+      } else if (this.currentHeading === SOUTH) {
+        this.currentHeading = EAST;
+      } else if (this.currentHeading === EAST) {
+        this.currentHeading = NORTH;
       }
     } else if (direction === RIGHT) {
-      if (this.currentHeading === "N") {
-        this.currentHeading = "E";
-      } else if (this.currentHeading === "E") {
-        this.currentHeading = "S";
-      } else if (this.currentHeading === "S") {
-        this.currentHeading = "W";
-      } else if (this.currentHeading === "W") {
-        this.currentHeading = "N";
+      if (this.currentHeading === NORTH) {
+        this.currentHeading = EAST;
+      } else if (this.currentHeading === EAST) {
+        this.currentHeading = SOUTH;
+      } else if (this.currentHeading === SOUTH) {
+        this.currentHeading = WEST;
+      } else if (this.currentHeading === WEST) {
+        this.currentHeading = NORTH;
       }
     } else {
       console.log("invalid heading instructions");
